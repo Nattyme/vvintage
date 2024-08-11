667a02056cb933ebdaa1ddbdbeb525e975c27d3b
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-08-11 15:33:27 +0300

    contact page yandex map base

--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,8 @@ Fancybox.bind('[data-fancybox]', {
 // Мобильная навигация
 import mobileNav from './modules/mobile-nav.js';
 import tab from './modules/tab.js';
+import yMap from './modules/ymap.js';
 mobileNav();
 tab();
+yMap();
+
