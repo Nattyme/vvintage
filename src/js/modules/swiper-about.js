455202081c29fea3444d8fe84ee9888ab42125c4
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-10-13 18:34:40 +0300

    main page swiper-about navigation base html and style

--- src/js/modules/swiper-about.js
+++ src/js/modules/swiper-about.js
@@ -1,16 +1,18 @@
 function swiperAbout () {
 
   const sliderAbout = new Swiper(".swiperAbout", {
-    autoplay:{
-      delay:	5000,
-      pauseOnMouseEnter: true,
-    },
+    // autoplay:{
+    //   delay:	5000,
+    //   pauseOnMouseEnter: true,
+    // },
     spaceBetween: 30,
     slidesPerView: 1,
     speed: 2000,
-    loop: true,
     // watchSlidesProgress: false,
-   
+    navigation: {
+      prevEl: ".swiperAbout-button-prev",
+      nextEl: ".swiperAbout-button-next",
+    },
   });
 }
 
