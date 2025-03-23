2152d3437c45d27ef2226cdf492e24c59e3ee8ac
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-03-23 22:50:10 +0300

    header: background-imgage replaced

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,6 +1,6 @@
 function swiperHeader () {
 
-  const swiperHeaderText = new Swiper(".swiperHeaderText", {
+  new Swiper(".swiperHeaderText", {
     // autoplay:{
     //   delay:	2000,
     //   pauseOnMouseEnter: true,
@@ -19,21 +19,6 @@ function swiperHeader () {
       crossFade: true     // added(resolve the overlapping of the slides)
     },  
   });
-
-  const swiperHeaderImg = new Swiper(".swiperHeaderImg", {
-    // watchSlidesProgress: false,
-    speed: 2000,
-    loop: true,
-    effect: 'fade',
-    fadeEffect: {           // added
-      crossFade: true     // added(resolve the overlapping of the slides)
-    },  
-    
-    // spaceBetween: 30,
-  
-  });
-
-  swiperHeaderText.controller.control = swiperHeaderImg;
 
 }
 
