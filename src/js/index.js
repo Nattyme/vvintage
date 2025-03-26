5c79350adfba2c0bb88d6266f38eff6b10a82af6
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-03-26 23:45:26 +0300

    main-page nav-menu layout correction

--- src/js/index.js
+++ src/js/index.js
@@ -3,24 +3,12 @@
 // import mobileNav from './modules/mobile-nav.js';
 import tab from './modules/tab.js';
 
-// swipers
-import swiperHeader from './modules/swiper.js';
-import swiperAbout from './modules/swiper-about.js';
-import swiperProduct from './modules/swiper-product.js';
-
 //fancybox
 import fancyBox from './modules/fancybox.js';
-
-// header bg change on scroll
-import headerTopChange from './modules/header-bg-change.js';
 import yMap from './modules/ymap.js';
 // mobileNav();
 
 tab();
-headerTopChange();
-swiperHeader();
-swiperAbout();
-swiperProduct();
 fancyBox();
 // yMap();
 // isMobile();
