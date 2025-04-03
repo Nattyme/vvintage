d7105499de988d1932c48c0eef96fb9c644fe1a8
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-04-03 15:34:45 +0300

    page-admin: control panel base layout and style correction

--- src/js/modules/addControlPanel.js
+++ src/js/modules/addControlPanel.js
@@ -2,6 +2,7 @@ import addAccordion from "./accordion";
 
 
 const addControlPanel = () => {
+  // Данные разделов панели управления 
   const data = [
     {
       'cat' : 'Админ панель',
@@ -28,13 +29,44 @@ const addControlPanel = () => {
       'icon' : 'settings',
       'subCat' : []
     },
-    
+    {
+      'cat' : 'Мой профиль',
+      'icon' : 'user',
+      'subCat' : []
+    },
+    {
+      'cat' : 'Выход',
+      'icon' : 'log-out',
+      'subCat' : []
+    }
   ];
 
+  const panel = document.querySelector('.control-panel__list');
+  const panelItemTemplate = (cat, icon) => {
+    return `
+            <li class="control-panel__list-item">
+              <button class="control-panel__list-button" title="Перейти в раздел '${cat}'">
+                <div class="control-panel__list-img-wrapper">
+                  <img class="control-panel__list-img" src="./../../img/svgsprite/stack/svg/sprite.stack.svg#${icon}" alt="Админ панель" />
+                </div>
+                ${cat}
+              </button>
+            </li>
+          `;
+  }
+
+  const panelTabTemplate = () => {
+
+  }
+
+  // Обходим массив с данными и создаем HTML разметку
+  const controlPanelList = data.map(item => panelItemTemplate(item.cat, item.icon)).join('');
+  if(!panel) return;
+  panel.insertAdjacentHTML('beforeend', controlPanelList);
+
+  // Запускаем функцию аккордеона
   addAccordion('many', '#control-panel-tab');
-  console.log('hey from control panel');
-  console.log(window.location);
-  
+ 
 }
 
 export default addControlPanel;
