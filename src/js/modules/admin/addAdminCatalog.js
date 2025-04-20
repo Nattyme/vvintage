2c002bbe2edbdcc44e01d47c82857ceff98cd721
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-04-20 14:17:25 +0300

    admin-page: subCat list diplay base logic

--- src/js/modules/admin/addAdminCatalog.js
+++ src/js/modules/admin/addAdminCatalog.js
@@ -4,16 +4,47 @@ import catalogData from './../../../data/categories.json';
 const addAdminCatalog = (catalogWrapper) => {
   const cats = JSON.parse(JSON.stringify(catalogData));
   const catalogList = document.querySelector(catalogWrapper);
-  const catalogCardsList = document.querySelector('#catalog-cards');
   // Запускаем функцию аккордеона
   setTimeout(() => {
     addAccordion('many', catalogWrapper); 
   }, 0);
 
+  // Ф-ция создает кнопки с ссылками
+  const getCategoryBlockLink = (url, dataBtn, icon) => {
+    return `
+      <a href="${url}" class="category-block__link" data-btn="${dataBtn}">
+        <svg class="icon icon--${icon}">
+          <use href="./img/svgsprite/sprite.symbol.svg#${icon}"></use>
+        </svg>
+      </a>
+    `;
+  }
+
+  const getCatalogItemSubCatTemplate = (subCats) => {
+    return subCats.map( subCatItem => {
+      if (subCatItem.name === 'Все категории') return; // Чтобы не добавлся объкет "Все категории" 
+      return `
+      <li class="catalog-list__sublist__item" dataId="${subCatItem.id}">
+        <button type="button" class="category-block category-block--sublist">
+          <span class="category-block__text text-ellipsis">
+          ${subCatItem.name}
+            <!-- <span class="category-block__counter">(5)</span> -->
+          </span>
+          <span class="category-block__action-links">
+            ${getCategoryBlockLink('edit.php', 'edit', 'edit')}
+            ${getCategoryBlockLink('delete.php', 'remove', 'remove')}
+          </span>
+        </button>
+      </li>
+    `;
+    }).join('');
+  }
+
   const getCatalogItemTemplate = (item) => {
+    const subCatsTemplate = getCatalogItemSubCatTemplate(item.subCats);
     return `
      <li class="catalog-list__item accordion__item text-ellipsis">
-          <button type="button" class="category-block accordion__btn" >
+          <button type="button" class="category-block accordion__btn" title="Открыть категорию ${item.name}">
             <span class="expand-icon">
               <span class="expand-icon__body"></span>
             </span>
@@ -22,26 +53,19 @@ const addAdminCatalog = (catalogWrapper) => {
               <!-- <span class="category-block__counter">(5)</span> -->
             </span>
             <span class="category-block__action-links">
-              <a href="edit.php" class="category-block__link" data-btn="edit">
-                <svg class="icon icon--edit">
-                  <use href="./img/svgsprite/sprite.symbol.svg#edit"></use>
-                </svg>
-              </a>
-              <a href="delete.php" class="category-block__link" data-btn="remove">
-                <svg class="icon icon--remove">
-                  <use href="./img/svgsprite/sprite.symbol.svg#remove"></use>
-                </svg>
-              </a>
+              ${getCategoryBlockLink('edit.php', 'edit', 'edit')}
+              ${getCategoryBlockLink('delete.php', 'remove', 'remove')}
             </span>
           </button>
-          <ul class="catalog-list__sublist accordion__content"></ul>
+          ${subCatsTemplate.length ? `<ul class="catalog-list__sublist accordion__content">${subCatsTemplate}</ul>` : ''}
         </li>
     `;
   }
 
+
+
   const catalogListTemplate = cats.map(cat => getCatalogItemTemplate(cat)).join('');
   catalogList.insertAdjacentHTML('beforeend', catalogListTemplate);
-console.log(catalogListTemplate);
 
 //   <li class="catalog-list__sublist__item">
 //   <button type="button" class="category-block category-block--sublist">
